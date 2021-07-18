eb24844a68fa62d4a02dfb6bd3f7390b87cce48a
Kuldeep Joshi <38746631+cooldeep5897@users.noreply.github.com>
2021-07-18 10:42:11 +0530

    new commits

---
+++ build/precache-manifest.a84eca6f921cec426f185e3d80832905.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a1c66ecbf15f3de1b11b7e22a30ff3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a981b6d357caa111da3f",
+    "url": "/portfolio/static/css/2.fb64bef1.chunk.css"
+  },
+  {
+    "revision": "0b9dd5b6e5ba15d19cf4",
+    "url": "/portfolio/static/css/main.604e9d5b.chunk.css"
+  },
+  {
+    "revision": "a981b6d357caa111da3f",
+    "url": "/portfolio/static/js/2.56032b1b.chunk.js"
+  },
+  {
+    "revision": "603f637d7d58e4667a575522771b0bbb",
+    "url": "/portfolio/static/js/2.56032b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b9dd5b6e5ba15d19cf4",
+    "url": "/portfolio/static/js/main.41625c0d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d56d3e6db9ade22884a69fe93eb4b455",
+    "url": "/portfolio/static/media/codechef.d56d3e6d.png"
+  },
+  {
+    "revision": "3018b587971c542f9e7b463af3d0e439",
+    "url": "/portfolio/static/media/dark_wall.3018b587.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e4eef3e4303ee803dbc44ab60e22f148",
+    "url": "/portfolio/static/media/github.e4eef3e4.png"
+  },
+  {
+    "revision": "a9510e92f330064ce16a1dac0b7611bc",
+    "url": "/portfolio/static/media/kuldeep2.a9510e92.jpg"
+  },
+  {
+    "revision": "8a34fa75300b7f9f87b4d0ba78dbbdc1",
+    "url": "/portfolio/static/media/leetcode.8a34fa75.PNG"
+  },
+  {
+    "revision": "33a32219df8fc8e22b1b853d9ac9caf8",
+    "url": "/portfolio/static/media/undraw_static_assets_rpm6.33a32219.svg"
+  }
+]);
